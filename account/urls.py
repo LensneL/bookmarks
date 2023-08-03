10cbe3c396d4513fc003a213edaeb94fa63a32b7
Lens <sviridov1005@mail.ru>
2023-08-03 18:40:16 +0300

    add redirect_url and create template dashboard

--- account/urls.py
+++ account/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
-
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout')
+
+    path('', views.dashboard, name='dashboard')
 ]
