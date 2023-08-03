e6e271f445fe90546d1ab492f7430be03c6f6a5c
Lens <sviridov1005@mail.ru>
2023-08-03 18:46:45 +0300

    add code in tempalte base.html

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('', views.dashboard, name='dashboard')
 ]
