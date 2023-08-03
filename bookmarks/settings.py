fc749d487ae5cb3fdfb346f0b7e8f2825e036a3c
Lens <sviridov1005@mail.ru>
2023-08-03 19:14:10 +0300

    change dashboard

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -30,14 +30,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
-
+    'django.contrib.admin',
 ]
 
 MIDDLEWARE = [
