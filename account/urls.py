3ab4b8bb1e013dc12ef37c23c11ed15ebe567fc7
Lens <sviridov1005@mail.ru>
2023-08-04 04:58:44 +0300

    add url and temp password change

--- account/urls.py
+++ account/urls.py
@@ -5,5 +5,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView(), name='password_change_done'),
     path('', views.dashboard, name='dashboard'),
+
 ]
