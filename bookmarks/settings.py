fd2cf4397070bae9c88c936324ad124c2a9290a1
Lens <sviridov1005@mail.ru>
2023-08-04 17:42:58 +0300

    add Media root and url

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -127,3 +127,7 @@ LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'dashboard'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
