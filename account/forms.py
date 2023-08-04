664d995902de1e16db7f9fe6a2b404a3d094829a
Lens <sviridov1005@mail.ru>
2023-08-04 12:01:07 +0300

    add register form and template

--- account/forms.py
+++ account/forms.py
@@ -7,7 +7,7 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
 
-class UserRegisterForm(forms.ModelForm):
+class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput),
 
--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegisterForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -35,12 +35,12 @@ def dashboard(request):
 
 def register(request):
     if request.method == 'POST':
-        user_form = UserRegisterForm(request.POST)
+        user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
             return render(request, 'account/register_done.html', {'new_user': new_user})
         else:
-            user_form = UserRegisterForm()
-    return render(request, 'account/register.html', {'user_form': user_form})
+            user_form = UserRegistrationForm()
+        return render(request, 'account/register.html', {'user_form': user_form})
