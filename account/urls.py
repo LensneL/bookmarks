427b7f2ddbe647aee1529dc434b7ddaf23186004
Lens <sviridov1005@mail.ru>
2023-08-04 05:16:08 +0300

    add shadow with click

--- account/urls.py
+++ account/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
     path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    path('password-change/done/', auth_views.PasswordChangeDoneView(), name='password_change_done'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('', views.dashboard, name='dashboard'),
 
 ]
