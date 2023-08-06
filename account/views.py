108fdeac56dec676eef3db51bea9cb2971364b38
Lens <sviridov1005@mail.ru>
2023-08-06 09:44:51 +0300

    add tamp edit and improved vie and dashboard

--- account/views.py
+++ account/views.py
@@ -66,4 +66,4 @@ def edit(request):
         profile_form = ProfileEditForm(instance=request.user.profile)
 
     return render(request, 'account/edit.html', {'user_form': user_form,
-                                                                     'profile_form': profile_form})
+                                                 'profile_form': profile_form})
