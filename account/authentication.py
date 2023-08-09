6ded2c2f64595da4badd94f92a6053576686473d
Lens <sviridov1005@mail.ru>
2023-08-09 14:58:02 +0300

    add new startapp images

--- account/authentication.py
+++ account/authentication.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from account.models import Profile
 
 
 class EmailAuthBackend:
@@ -18,3 +19,7 @@ def get_user(self, user_id):
         return User.objects.get(pk=user_id)
     except User.DoesNotExist:
         return None
+
+
+def create_profile(backend, user, *args, **kwargs):
+    Profile.objects.get_or_create(user=user)
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -150,6 +150,7 @@ SOCIAL_AUTH_PIPELINE = [
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
