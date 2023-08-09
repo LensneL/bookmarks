fff4a35e2971fb4aeaf03890f42dfaa666c70592
Lens <sviridov1005@mail.ru>
2023-08-09 15:26:33 +0300

    improve models images

--- images/models.py
+++ images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 
 
 class Image(models.Model):
@@ -7,17 +8,25 @@ class Image(models.Model):
                              related_name='images_created',
                              on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200,
-                            blank=True)
-    url = models.URLField(max_length=200)
+    slug = models.SlugField(max_length=200, blank=True)
+    url = models.URLField(max_length=2000)
     image = models.ImageField(upload_to='images/%Y/%m/%d/')
-    descriptions = models.TextField(blank=True)
+    description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True)
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                        related_name='images_liked',
+                                        blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     class Meta:
         indexes = [
-            models.Index(fields=['-created'])
+            models.Index(fields=['-created']),
         ]
+
         ordering = ['-created']
 
     def __str__(self):
