d51211a3c169f88a9f8a551c21e44849432b8524
Lens <sviridov1005@mail.ru>
2023-08-09 16:09:04 +0300

    add save in forms(images)

--- images/froms.py
+++ images/froms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Image
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+import requests
 
 
 class ImageCreateForm(forms.ModelForm):
@@ -18,4 +21,15 @@ class ImageCreateForm(forms.ModelForm):
             raise forms.ValidationError('The given URL does not match valid image extensions')
 
         return url
-                
+
+    def save(self, commit=True, force_insert=False, force_update=False):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+        response = requests.get(image_url)
+        image.image.save(image_name, ContentFile(response.content), save=False)
+        if commit:
+            image.save()
+        return image
