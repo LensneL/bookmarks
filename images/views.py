1c11af692b47620965637705e451a4be46ab68cb
Lens <sviridov1005@mail.ru>
2023-08-09 16:12:25 +0300

    rename forms

--- images/views.py
+++ images/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
 
-# Create your views here.
