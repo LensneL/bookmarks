fdd07639b18680339efe3912efb33f929b0d87b6
Lens <sviridov1005@mail.ru>
2023-08-10 06:55:43 +0300

    add js func bookmarkletLaunch

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -8,3 +8,33 @@ link.rel = 'stylesheet';
 link.type = 'text/css';
 link.href = styleUrl + '?r=' + Math.floor(Math.random() * 9999999999999999);
 head.appendChild(link);
+var body = document.getElementsByTagName('body')[0];
+boxHtml = '
+    <div id="bookmarklet">
+        <a href="#" id="close">&times;</a>
+        <h1>Select an image to bookmark:</h1>
+        <div class="images"></div>
+    </div>';
+body.innerHTML += boxHtml;
+
+function bookmarkletLaunch() {
+    bookmarklet = document.getElementById('bookmarklet');
+    var imagesFound = bookmarklet.querySelector('.images');
+    imagesFound.innerHTML = '';
+    bookmarklet.style.display = 'block';
+    bookmarklet.querySelector('#close')
+        .addEventListener('click', function () {
+            bookmarklet.style.display = 'none'
+            images = document.querySelectorAll('img[src$=".jpg"], img[src$=".jpeg"], img[src$=".png"]');
+            images.forEach(image => {
+                if (image.naturalWidth >= minWidth
+                    && image.naturalHeight >= minHeight) {
+                    var imageFound = document.createElement('img');
+                    imageFound.src = image.src;
+                    imagesFound.append(imageFound);
+                }
+            })
+        }
+}
+
+bookmarkletLaunch();
