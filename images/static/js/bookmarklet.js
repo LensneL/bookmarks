a9d38a8d0657921bdb14231ad388145bd6495108
Lens <sviridov1005@mail.ru>
2023-08-10 06:45:25 +0300

    add static js and css

---
+++ images/static/js/bookmarklet.js
@@ -0,0 +1,10 @@
+const siteUrl = '//127.0.0.1:8000/';
+const styleUrl = siteUrl + 'static/css/bookmarklet.css';
+const minWidth = 250;
+const minHeight = 250;
+var head = document.getElementsByTagName('head')[0];
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = styleUrl + '?r=' + Math.floor(Math.random() * 9999999999999999);
+head.appendChild(link);
