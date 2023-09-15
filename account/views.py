3097d2e1bac3574162b0cfc45c1c6836f4c5f5fe
Lens <sviridov1005@mail.ru>
2023-09-15 21:28:44 +0300

    add new funk user_list and user_detail

--- account/views.py
+++ account/views.py
@@ -6,6 +6,8 @@ from .forms import LoginForm, UserRegistrationForm, \
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 
 
 def user_login(request):
@@ -71,3 +73,22 @@ def edit(request):
 
     return render(request, 'account/edit.html', {'user_form': user_form,
                                                  'profile_form': profile_form})
+
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(request, 'account/user/list.html',
+                  {'section': 'people',
+                   'users': users})
+
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User,
+                             username=username,
+                             is_active=True)
+    return render(request,
+                  'account/user/detail.html',
+                  {'section': 'people',
+                   'user': user})
