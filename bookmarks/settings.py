3707dbb209701592be8e8e15f3fec467b1e9af41
Lens <sviridov1005@mail.ru>
2023-09-25 12:56:44 +0300

    connect to redis

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'social_django',
     'django_extensions',
     'easy_thumbnails',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -171,3 +173,9 @@ ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail',
                                         args=[u.username])
 }
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
