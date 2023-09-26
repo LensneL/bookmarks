f1dbfb36713468860c80b76112a83defdf58e03f
Lens <sviridov1005@mail.ru>
2023-09-26 10:51:54 +0300

    add new func image_ranking

--- images/views.py
+++ images/views.py
@@ -10,6 +10,8 @@ from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, \
     PageNotAnInteger
 from action.utils import create_action
+import redis
+from django.conf import settings
 
 
 @login_required
@@ -35,9 +37,12 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr(f'image{image.id}:views')
+    r.zincrby('image_ranking', 1, image.id)
     return render(request, 'images/image/detail.html',
                   {'section': 'images',
-                   'image': image})
+                   'image': image,
+                   'total_views': total_views})
 
 
 @login_required
@@ -82,3 +87,19 @@ def image_list(request):
                   {'section': 'images',
                    'images': images})
 
+
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB)
+
+
+@login_required
+def image_ranking(request):
+    image_ranking = r.zrange('image_ranking', 0, 1,
+                             desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request, 'images/image/ranking.html',
+                  {'section': 'image',
+                   'most_viewed': most_viewed})
